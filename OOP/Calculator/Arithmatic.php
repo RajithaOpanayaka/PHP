cb2c3e32706d5f49aed982edcc8740c4a4ed3966
RajithaOpanayaka <rajithaopanayaka.ro@gmail.com>
2021-06-05 00:40:59 +0530

    static methods and properties

---
+++ OOP/Calculator/Arithmatic.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class Arithmatic{
+
+    public static $welcomeMsg="Welcome";
+
+    public static function add($num1,$num2){
+        return $num1+$num2;
+    }
+    public static function substract($num1,$num2){
+        return $num1-$num2;
+    }
+}
+
+?>
---
+++ OOP/Calculator/Calculator.php
@@ -0,0 +1,7 @@
+<?php
+require_once "Arithmatic.php";
+
+echo Arithmatic::add(1,2);
+echo Arithmatic::$welcomeMsg;
+
+?>
